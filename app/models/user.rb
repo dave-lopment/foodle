5a977f4ac77c8fe86f06232e0beb8e9a5e37bd25
Benjamin Guttmann <bguttmann@avarteq.de>
2015-07-27 15:04:13 +0200

    added tests for user-model, added validation for user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, allow_blank: false
-  validates :email_address, uniqueness: true 
+  validates :email_address, uniqueness: true, presence: true, allow_blank: false 
+  validates :postal, length: { is: 5 }
+  validates :city, presence: true, allow_blank: false
+  validates :street, presence: true, allow_blank: false
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,54 @@ describe User do
     user = build(:user, name: '')
     expect(user).not_to be_valid
   end
-  it 'expect postal code length equal to 5'
+  it 'expect postal code not to be nil' do
+    user = build(:user, postal: '')
+    expect(user).not_to be_valid
+  end
+  it 'expect postal code not to be blank' do
+    user = build(:user, postal: '   ')
+    expect(user).not_to be_valid
+  end
+  it 'expect postal code length not to be less than 5' do
+    user = build(:user, postal: "1234")
+    expect(user).not_to be_valid
+  end
+  it 'expect postal code length not to be greater than 5' do
+    user = build(:user, postal: "234235")
+    expect(user).not_to be_valid
+  end
+  it 'expect postal code length to be 5' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  it 'expect email to have right form'
+  it 'expect email not to be nil' do
+    user = build(:user, email_address: '')
+    expect(user).not_to be_valid
+  end
+  it 'expect email not to be blank' do
+    user = build(:user, email_address: '   ')
+    expect(user).not_to be_valid
+  end
   it 'expect email_address to be unique' do
     userA = create(:user, email_address: 'abc@example.org') # user must be saved in the db
     userB = build(:user, email_address: 'abc@example.org') # new user with same email_address
     expect(userB).not_to be_valid
   end
-  it 'expect street not to be nil or blank'
-  it 'expect city not to be nil or blank'
+  it 'expect street not to be nil' do
+     user = build(:user, street: '')
+     expect(user).not_to be_valid
+  end
+  it 'expect street not to be blank' do
+     user = build(:user, street: '   ')
+     expect(user).not_to be_valid
+  end
+  it 'expect city not to be nil' do
+     user = build(:user, city: '')
+     expect(user).not_to be_valid
+  end
+  it 'expect city not to be blank' do
+     user = build(:user, city: '   ')
+     expect(user).not_to be_valid
+  end
 end
