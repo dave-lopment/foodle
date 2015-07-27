16f6a8ebc4db534c508db5a987489db9428b51e5
David Landry <dlandry@avarteq.de>
2015-07-27 12:13:37 +0200

    add some user specs and new gem factory_girl_rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, allow_blank: false
+  validates :email_adress, uniqueness: true 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User do
+  it {should_not be_valid}
+
+  it "name should not be blank" do
+    subject.email_adress = "abc@abc.de"
+    subject.name = "    "
+    expect(subject).not_to be_valid
+  end
+
+
+end
