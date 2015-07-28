c0915d49fbd0bae37711a57ec7abd0ed0c5eff9c
David Landry <dlandry@avarteq.de>
2015-07-28 09:40:38 +0200

    add gem devise

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 describe Article do
   it "expect not to be valid when name is blank" do
-    article = FactoryGirl.build(:article, name: ' ')
+    article = build(:article, name: ' ')
     expect(article).not_to be_valid
   end
 
   it "expect not to be valid when no price" do
-    article = FactoryGirl.build(:article, price: ' ')
+    article = build(:article, price: ' ')
     expect(article).not_to be_valid
   end
 
   it "expect not to be valid when price less than 0" do
-    article = FactoryGirl.build(:article, price: -50)
+    article = build(:article, price: -50)
     expect(article).not_to be_valid
   end
 
   it "expect price to have correct format (number.xx)" do
-    article = FactoryGirl.build(:article, price: 10.123)
+    article = build(:article, price: 10.123)
     expect(article).not_to be_valid
   end
 end
