f2973a8dc33b50e12efcd51707c99d74766183b0
David Landry <dlandry@avarteq.de>
2015-07-28 09:19:51 +0200

    add some specs for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  validates :name, presence: true
+  validates :price, :numericality => {:greater_than => 0, :less_than => 1000}
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article do
+  it "expect not to be valid when name is blank" do
+    article = FactoryGirl.build(:article, name: ' ')
+    expect(article).not_to be_valid
+  end
+
+  it "expect not to be valid when no price" do
+    article = FactoryGirl.build(:article, price: ' ')
+    expect(article).not_to be_valid
+  end
+
+  it "expect not to be valid when price less than 0" do
+    article = FactoryGirl.build(:article, price: '-50')
+    expect(article).not_to be_valid
+  end
 end
