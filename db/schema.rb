c6e7876e925ff9a13f955098e20da0bcfbcf12a0
David Landry <dlandry@avarteq.de>
2015-07-28 08:32:23 +0200

    add article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727104442) do
+ActiveRecord::Schema.define(version: 20150728062047) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.boolean  "isVegetarian"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/articles.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :article do
+    name "MyString"
+price 1
+description "MyString"
+isVegetarian false
+  end
+
+end
