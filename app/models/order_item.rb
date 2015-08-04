3988509488de4139a68dce8219940dd3c856cd94
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-04 14:23:18 +0200

    Add Rspec examples for order_item validation

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,5 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+
 end
--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -1,10 +1,5 @@
 FactoryGirl.define do
   factory :order_item do
-    product nil
-order nil
-unit_price "9.99"
-quantity 1
-total_price "9.99"
+    quantity 1
   end
-
 end
