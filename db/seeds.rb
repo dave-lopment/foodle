adf341b46061fa316557f887827e47d6762fdea0
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-04 13:22:18 +0200

    Add Seeds for OrderStatus, modify order

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,11 @@
                   price: price,
                   isVegetarian: false)
 end
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progess"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled" 
+
+
