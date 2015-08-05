a1760fae554edbab255a26deed7df0c017c88374
David Landry <dlandry@avarteq.de>
2015-08-05 10:26:30 +0200

    add users and one admin to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,40 @@
 require 'ffaker'
 
 Article.delete_all
+User.delete_all
+
 30.times do |n|
   name = FFaker::Food.meat
   description = FFaker::Food.ingredient
   price = n + 1
-  Article.create(name: name,
+  Article.create!(name: name,
                   description: description,
                   price: price,
                   isVegetarian: false)
 end
+
+40.times do |n|
+  name = FFaker::Name.name
+  password = "12345678"
+  street = FFaker::AddressDE.street_name
+  city = FFaker::AddressDE.city
+  postal = FFaker::AddressDE.zip_code
+  email = "#{n}" << FFaker::Internet.email
+
+  admin = false
+  User.create!( name: name,
+                password: password,
+                street: street,
+                city: city,
+                postal: postal,
+                email: email,
+                admin: admin)
+end
+
+User.create!( name: "Peter Lustig",
+              password: "12345678",
+              street: "Der Admin-Garten",
+              city: "Stadtmin",
+              postal: "11111",
+              email: "peter@lustig.de",
+              admin: true)
