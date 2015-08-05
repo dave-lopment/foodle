06da593f55c1e294699dd9eb3c786892bf6cdf83
David Landry <dlandry@avarteq.de>
2015-08-05 11:19:34 +0200

    add delete user for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    #flash[:success] = "User deleted"
+    redirect_to users_path
+  end
 end
