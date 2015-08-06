541834eb6a78a741aadd8d7c31869b54ae8cf335
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-06 15:08:28 +0200

    Solve some small issues with the shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def current_order
     if !session[:order_id].nil?
-      Order.find(session[:order_id]).nil?
+      Order.find(session[:order_id])
     else
       Order.new
     end
