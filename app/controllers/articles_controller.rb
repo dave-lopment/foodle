7039e551740edf5002d427e28ebf71ad95e19bbc
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-06 13:17:19 +0200

    Add some layout changes for the shopping cart

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.paginate(:page => params[:page], :per_page => 10)
+    @order_item = current_order.order_items.new
   end
 
   # upvote with help of acts_as_votable
