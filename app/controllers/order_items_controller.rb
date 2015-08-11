fdde04dbe7ee254295bf584ed7982dc7098b9feb
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-11 13:02:34 +0200

    Fix Bug with Article Quantities

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,13 +1,21 @@
 class OrderItemsController < ApplicationController
   def create
     @order = current_order
-    @order_item = @order.order_items.new(order_item_params)
-    @order.save
-    session[:order_id] = @order.id
+    @order_item = @order.order_items.find_by(article_id: order_item_params[:article_id])
+    if @order_item.nil? 
+      then
+        @order_item = @order.order_items.new(order_item_params)
+        @order.save
+        session[:order_id] = @order.id
+      else
+        update_param = order_item_params
+        update_param[:quantity] = update_param[:quantity].to_i + @order_item.quantity.to_i
+        @order_item.update_attributes(update_param) 
+        @order_items = @order.order_items
+      end
   end
 
   def update
-    print "I am in update right now"
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
@@ -26,4 +34,5 @@ class OrderItemsController < ApplicationController
     def order_item_params
        params.require(:order_item).permit(:quantity, :article_id)
     end
+
 end
