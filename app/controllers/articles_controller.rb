ee9e02440927c46d251f922ce02ab03e9dd5d194
David Landry <dlandry@avarteq.de>
2015-08-12 09:41:58 +0200

    can choose category in article new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,6 +50,6 @@ class ArticlesController < ApplicationController
   private
 
     def article_params
-      params.require(:article).permit(:name, :description, :price, :isVegetarian)
+      params.require(:article).permit(:name, :description, :price, :isVegetarian, :category_id)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -10,5 +10,6 @@ class Article < ActiveRecord::Base
   validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ },
     :numericality => {:greater_than => 0, :less_than => 1000}
   validates :description, presence: true
+  validates :category_id, presence: true 
 
 end
