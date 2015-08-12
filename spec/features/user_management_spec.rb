1c9bfcaa3be34fc2271c262a435d04165dd1dbcc
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-12 13:55:17 +0200

    Add Integration Test for User_Management

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 feature "the login process" do 
   
-  scenario "signs me in" do
-    page.visit root_url
-    expect(page).to have_content "Willkommen bei Foodle!"
+  scenario "sign in with wrong credentials" do
+    visit new_user_session_path
+    expect(page).to have_content 'Anmeldung'
+    within("#new_user") do
+      fill_in 'E-Mail', :with => 'user@example.de'
+      fill_in 'Passwort', :with => '12345678'
+    end
+    click_button 'Anmelden'
+    expect(page).to have_content 'Invalid email or password'
   end
+
 end
       
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
-  it 'expect subtotal with quantity 1 to be correct' do
-    article = create(:article , price: 1.00)
-    expect(article).to be_valid
-    order = Order.create()
-    order_item = create(:order_item, {article_id: article, order_id: order})
-    expect(order.subtotal).to be_equal(1.00)
-  end
+  it 'expect subtotal with quantity 1 to be correct'
 
   it 'expect count of different elements to be correct'
 
