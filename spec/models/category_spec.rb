beae09c144eb1cde2efd3ee1284240f1f4e538cf
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-14 11:53:50 +0200

    Add Context to Category-Model Tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 describe Category do
+
   it "should be valid with normal attributes" do
     category = build(:category)
     expect(category).to be_valid
   end
 
-  it "should be invalid when name is nil" do
-    category = build(:category, name: nil)
-    expect(category).not_to be_valid
-  end
-
-  it "should be invalid when name is blank" do
-    category = build(:category, name: '  ')
-    expect(category).not_to be_valid
-  end
-
-  it "should be invalid if name too long" do
-    category = build(:category, name: 'a'*33)
-    expect(category).not_to be_valid
+  context "Name Attribute Tests" do
+    it "should be invalid when name is nil" do
+      category = build(:category, name: nil)
+      expect(category).not_to be_valid
+    end
+ 
+    it "should be invalid when name is blank" do
+      category = build(:category, name: '  ')
+      expect(category).not_to be_valid
+    end
+ 
+    it "should be invalid if name too long" do
+      category = build(:category, name: 'a'*33)
+      expect(category).not_to be_valid
+    end
   end
 
   context "destroy" do
@@ -33,4 +36,5 @@ describe Category do
       expect{Category.destroy(@category)}.to change(Article, :count).from(2).to(0)
     end
   end
+
 end
