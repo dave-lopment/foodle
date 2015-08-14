6950da0d6b71ea79383782e3e9af85e9e8f1a372
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-14 11:52:16 +0200

    Add Context to Article-Model Tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,39 +6,45 @@ describe Article do
     expect(article).to be_valid
   end
 
-  it "ecpect not to be valid when name is nil" do
-    article = build(:article, name: nil)
-    expect(article).not_to be_valid
+  context "Name Attribute Tests" do
+    it "expect not to be valid when name is nil" do
+      article = build(:article, name: nil)
+      expect(article).not_to be_valid
+    end
+ 
+    it "expect not to be valid when name is blank" do
+      article = build(:article, name: '    ')
+      expect(article).not_to be_valid
+    end
   end
 
-  it "expect not to be valid when name is blank" do
-    article = build(:article, name: '    ')
-    expect(article).not_to be_valid
+  context "Price Attribute Tests" do
+    it "expect not to be valid when no price" do
+      article = build(:article, price: nil)
+      expect(article).not_to be_valid
+    end
+ 
+    it "expect not to be valid when price less than 0" do
+      article = build(:article, price: -50)
+      expect(article).not_to be_valid
+    end
+ 
+    it "expect price to have correct format (number.xx)" do
+      article = build(:article, price: 10.123)
+      expect(article).not_to be_valid
+    end
   end
-
-  it "expect not to be valid when no price" do
-    article = build(:article, price: nil)
-    expect(article).not_to be_valid
-  end
-
-  it "expect not to be valid when price less than 0" do
-    article = build(:article, price: -50)
-    expect(article).not_to be_valid
-  end
-
-  it "expect price to have correct format (number.xx)" do
-    article = build(:article, price: 10.123)
-    expect(article).not_to be_valid
-  end
-
-  it "expet to be invalid when description is nil" do
-    article = build(:article, description: nil)
-    expect(article).not_to be_valid
-  end
-
-  it "expect to be invalid when description is blank" do
-    article = build(:article, description: '   ')
-    expect(article).not_to be_valid
+  
+  context "Description Attribute Tests" do
+    it "expet to be invalid when description is nil" do
+      article = build(:article, description: nil)
+      expect(article).not_to be_valid
+    end
+ 
+    it "expect to be invalid when description is blank" do
+      article = build(:article, description: '   ')
+      expect(article).not_to be_valid
+    end
   end
 
 end
