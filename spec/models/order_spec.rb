010a8d3bcfc631a95b602da6b76fa578e8cfc0f2
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-17 11:11:49 +0200

    Add Tests for Order-Model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,17 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
-  it 'expect subtotal with quantity 1 to be correct'
-
-  it 'expect count of different elements to be correct'
-
-  it 'expect count of elements with higher quantity then 1 to be correct'
-
-  it 'expect order_status not to be nil' do
-    order = Order.create()
-    expect(order[:order_status_id]).not_to be_nil
+  context 'Order Tests' do
+    it 'expect subtotal with quantity 1 to be correct'#do
+     # orderItem1 = FactoryGirl.create(:order_item,{quantity: 1, unit_price: 2.0})
+     # order = FactoryGirl.create(:order, order_items: [orderItem1])
+     # expect(order.subtotal).to be_equal(2.0)
+   # end
+  
+    it 'expect count of different elements to be correct' do
+      orderItem1 = FactoryGirl.create(:order_item)
+      orderItem2 = FactoryGirl.create(:order_item, quantity: 3)
+      order = FactoryGirl.create(:order, order_items: [orderItem1, orderItem2])
+      expect(order.count).to be_equal(4)
+    end
+  
+    it 'expect count of elements with higher quantity then 1 to be correct' do
+      orderItem1 = FactoryGirl.create(:order_item, quantity: 5)
+      order = FactoryGirl.create(:order, order_items: [orderItem1])
+      expect(order.count).to be_equal(5)
+    end
+  
+    it 'expect order_status not to be nil' do
+      order = Order.create()
+      expect(order[:order_status_id]).not_to be_nil
+    end
+  
+    it 'expect subtotal updated after save'
   end
-
-  it 'expect subtotal updated after save'
-
 end
