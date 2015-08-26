445d00caeba6a8cf8914f3a37b35b2a8c0e02645
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-26 11:44:22 +0200

    Change time for automated deletion to 2 days

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
     @orders.each do |order|
-      if order.Erstellung? && (order.updated_at > 2.minutes.ago)
+      if order.Erstellung? && (order.updated_at > 2.days.ago)
         Order.find(order[:id]).destroy
       end
     end
