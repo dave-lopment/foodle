518410054f6f265dac3e8600f5c0356901535c83
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-28 07:16:37 +0200

    Add test for order management user facing

--- spec/features/order_management_user_facing_spec.rb
+++ spec/features/order_management_user_facing_spec.rb
@@ -1,69 +1,65 @@
 require 'rails_helper'
 
-#TODO Put log_in_as(user) and set_order_status to a test_helper file
+feature 'order management user facing' do
 
-feature "order management admin facing" do 
-  let!(:order2) {create(:order)} 
+  #TODO Put log_in_as(user) and set_order_status to a test_helper file
+
   def log_in_as(user)
     visit new_user_session_path
     expect(page).to have_content 'Anmeldung'
     within("#new_user") do
       fill_in 'E-Mail', :with => user.email
-      fill_in 'Passwort', :with =>user.password
+      fill_in 'Passwort', :with => user.password
     end
     click_button 'Anmelden'
-    visit orders_path
+    visit meine_bestellungen_path
   end
 
-  def set_order_status(order,status_id)
+  def set_order_status(order, status_id)
     order.order_status = status_id
     order.reload
-    visit orders_path
+    visit meine_bestellungen_path
   end
 
-  context "admin" do
+  context "user" do
     before(:each) do
-      log_in_as(create(:user, admin: true))
+      log_in_as(create(:user))
       @order = create(:order)
-    end
+    erd
 
     context "show only orders with correct status" do
-
-      scenario "admin does not see any orders with status 'Erstellung'" do
+      scenario "user does not see any orders with status 'Erstellung'" do
         expect(page).not_to have_content 'Erstellung'
       end
  
-      scenario "admin does see orders with status 'Auftrag'" do
+      scenario "user does see orders with status 'Auftrag'" do
         set_order_status(order2, 1)
         expect(page).to have_content 'Auftrag'
       end
  
-      scenario "admin does see orders with status 'Abgeschickt'" do
+      scenario "user does see orders with status 'Abgeschickt'" do
         set_order_status(@order, 2)
         expect(page).to have_content 'Abgeschickt'
       end
  
-      scenario "admin does see orders with status 'Angekommen'" do
+      scenario "user does see orders with status 'Angekommen'" do
         set_order_status(@order, 3)
         expect(page).to have_content 'Angekommen'
       end
  
-      scenario "admin does see orders with status 'Storniert'" do
+      scenario "user does see orders with status 'Storniert'" do
         set_order_status(@order, 4)
         expect(page).to have_content 'Storniert'
       end
 
     end
+
     context "Test if all links, buttons and functions are there" do
       before(:each) do
         set_order_status(@order,1)
       end
 
-      scenario "admin sees delete button"
- 
-      scenario "after clicking delete button, order is gone"
- 
-      scenario "admin sees detail link" do
+      scenario "user sees detail link" do
         expect(page).to have_link 'Details'
       end
  
@@ -72,18 +68,17 @@ feature "order management admin facing" do
         expect(page).to render(show_order_path)
       end
  
- 
-      scenario "admin sees 'Liefern' button" do 
-        expect(page).to have_content 'Liefern'
+      scenario "user sees 'Stornieren' button" do 
+        expect(page).to have_content 'Stornieren'
       end
  
-      scenario "after clicking 'Liefern' Status changed" do
-        click_on 'Liefern'
-        expect(page).to have_content 'Abgeschickt'
+      scenario "after clicking 'Stornieren' Status changed" do
+        click_on 'Stornieren'
+        expect(page).to have_content 'Storniert'
       end
     end
 
-    context "Admin's order detail view" do
+    context "User's order detail view" do
 
       before(:each) do
         set_order_status(@order,1)
