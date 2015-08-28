362b7b8ae9e8480829e052c19a185fa30146e190
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-28 09:28:29 +0200

    Refactoring order_management_admin_facing still not working properly

--- spec/features/order_management_user_facing_spec.rb
+++ spec/features/order_management_user_facing_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'order management user facing' do
-
+  let(:order) {create(:order, {order_items: [order_item], user_id: [user.id]})}
+  let(:order_item){create(:order_item)}
+  let(:user) {create(:user)}
   #TODO Put log_in_as(user) and set_order_status to a test_helper file
 
   def log_in_as(user)
@@ -17,14 +19,14 @@ feature 'order management user facing' do
 
   def set_order_status(order, status_id)
     order.order_status = status_id
+    order.save!
     order.reload
     visit meine_bestellungen_path
   end
 
   context "user" do
     before(:each) do
-      log_in_as(create(:user))
-      @order = create(:order)
+      log_in_as(user)
     end
 
     context "show only orders with correct status" do
@@ -33,22 +35,22 @@ feature 'order management user facing' do
       end
  
       scenario "user does see orders with status 'Auftrag'" do
-        set_order_status(order2, 1)
+        set_order_status(order, 1)
         expect(page).to have_content 'Auftrag'
       end
  
       scenario "user does see orders with status 'Abgeschickt'" do
-        set_order_status(@order, 2)
+        set_order_status(order, 2)
         expect(page).to have_content 'Abgeschickt'
       end
  
       scenario "user does see orders with status 'Angekommen'" do
-        set_order_status(@order, 3)
+        set_order_status(order, 3)
         expect(page).to have_content 'Angekommen'
       end
  
       scenario "user does see orders with status 'Storniert'" do
-        set_order_status(@order, 4)
+        set_order_status(order, 4)
         expect(page).to have_content 'Storniert'
       end
 
@@ -56,7 +58,7 @@ feature 'order management user facing' do
 
     context "Test if all links, buttons and functions are there" do
       before(:each) do
-        set_order_status(@order,1)
+        set_order_status(order,1)
       end
 
       scenario "user sees detail link" do
@@ -64,8 +66,8 @@ feature 'order management user facing' do
       end
  
       scenario "after clicking details button, show renders" do
-        click_on 'Details'
-        expect(page).to render(show_order_path)
+        click_link 'Details'
+        expect(current_path).to eq(order_path(order.id))
       end
  
       scenario "user sees 'Stornieren' button" do 
@@ -73,7 +75,7 @@ feature 'order management user facing' do
       end
  
       scenario "after clicking 'Stornieren' Status changed" do
-        click_on 'Stornieren'
+        click_link 'Stornieren'
         expect(page).to have_content 'Storniert'
       end
     end
@@ -81,8 +83,8 @@ feature 'order management user facing' do
     context "User's order detail view" do
 
       before(:each) do
-        set_order_status(@order,1)
-	@order_item = @order.order_items.first
+        set_order_status(order,1)
+	@order_item = order.order_items.first
         click_on 'Details'
       end
 
@@ -95,7 +97,7 @@ feature 'order management user facing' do
       end
  
       scenario "Detail View got a column for article name" do
-	expect(page).to have_content @order_item.name
+	expect(page).to have_content @order_item.article.name
       end
 
       scenario "Detail View got a column for total price" do
