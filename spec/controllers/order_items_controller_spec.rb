3e08550032dba714c2c25443519609677668bdb9
Benjamin Guttmann <bguttmann@avarteq.de>
2015-08-31 07:17:55 +0200

    Add some more tests, fix some small session-bug

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -4,12 +4,34 @@ require 'rails_helper'
  
   describe "POST  #create" do
 
-	  it "add some examples"
   end
 
   describe "POST #update" do
+	let(:article){create(:article)
+	let(:order_item){create(:order_item, {quantity: 3, article_id: [article]})}
+	let(:order){create(:order, order_items: [order_item])}
+	}
+	context "admin" do
+          it "does not update the quantity of the article"
+	end
 
-	it "add some examples"
+	context "user" do
+	before(:each) do
+          sign_in(create(:user))
+	end
+          it "updates the quantity of the article" do
+	    put :update, :article_id => article.id, :quantity => 2
+	    order_item.reload
+	    expect(order_item.quantity).to eq(2)
+	  end
+
+	end
+
+	context "no user" do
+
+          it "does update the quantity of the article" 
+
+	end
 
   end
 
@@ -17,15 +39,29 @@ require 'rails_helper'
           
 	context "admin" do
 
-	  it "deletes an order item" do
+	  # could not be found because in the controller we are using current_order
+	  it "could not deletes an order item" do
             sign_in(create(:user, admin: true))
 	    order_item = create(:order_item)
-	    expect{ delete :destroy, :id => order_item}.to change(OrderItem, :count).by(-1)
+	    current_order = create(:order, order_items: [order_item])
+	    current_order.reload
+	    order_item.reload
+	    expect{ delete :destroy, :id => order_item}.to change(current_order, :count).by(-1)
 	  end
 
 	end
 
-	  it "add some examples"
+	context "user" do
+
+          it "deletes an order item" 
+
+	end
+
+        context "no user" do
+        
+          it "deletes an order item" 
+
+	end
 
   end
 
