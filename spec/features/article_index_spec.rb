a63c7125dba2732a707d761a75a7ed116760eed9
David Landry <dlandry@avarteq.de>
2015-09-01 10:21:39 +0200

    add feature specs for articleindex

--- spec/features/article_index_spec.rb
+++ spec/features/article_index_spec.rb
@@ -1,40 +1,49 @@
 require 'rails_helper'
 
 feature "Admin logs in and sees the 'Stammdaten' page (article#index)" do
+	let(:admin){FactoryGirl.create(:user, admin: true)}
+	let(:category){create(:category)}
+	let!(:article){create(:article, category_id: category.id)}
 
 	def log_in_as(user)
-	   	visit new_user_session_path
-	   	expect(page).to have_content 'Anmeldung'
-	    within("#new_user") do
-	      	fill_in 'E-Mail', :with => user.email
-	      	fill_in 'Passwort', :with =>user.password
-	    end
-	  	  click_button 'Anmelden'
+   	visit new_user_session_path
+   	expect(page).to have_content 'Anmeldung'
+    within("#new_user") do
+      	fill_in 'E-Mail', :with => user.email
+      	fill_in 'Passwort', :with =>user.password
+    end
+  	click_button 'Anmelden'
   	end
 
-  	def create_dummy_article
-  		@article = create(:article)
-  	end 
-	
-
-	let(:admin){FactoryGirl.create(:user, admin: true)}
-	let(:article){FactoryGirl.create(:article)}
-
-	before(:each) do 
+	before(:each) do
 		# loggin in as admin redirects to the Stammdaten page!
 		log_in_as(admin)
 		visit articles_path
-	end 
+	end
 
-	scenario "he sees categories' buttons"
+	scenario "he sees categories' buttons" do
+		expect(page).to have_link("Alle", :href => articles_path)
+		expect(page).to have_link("#{category.name}", articles_path(filter: "#{category.id}"))
+	end
 
-	scenario "he sees articles"
+	scenario "he sees articles" do
+		expect(page).to have_content "#{article.name}"
+	end
 
-	scenario "he sees edit buttons for articles"
+	scenario "he sees edit buttons for articles" do
+		expect(page).to have_link("", edit_article_path("#{article.id}"))
+	end
 
-	scenario "he sees delete buttons for articles"
+	scenario "he can delete articles" do
+		expect(page).to have_link('destroy')
+		expect { click_link 'destroy' }.to change(Article, :count).by(-1)
+	end
 
-	scenario "he sees pagination for articles"
+	scenario "he sees pagination for articles" do
+		44.times {create(:article, category_id: category.id)}
+		visit articles_path
+		expect(page).to have_css('div.pagination')
+	end
 
 
-end 
+end
